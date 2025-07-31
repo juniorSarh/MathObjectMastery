877ce1c6d45644c276531ed39a52fea96ff3db26
JuniorSarh <sabelohgumede@gmail.com>
2025-07-31 15:17:20 +0200

    added readme img

--- Math-Work.js
+++ Math-Work.js
@@ -1,14 +1,11 @@
 
 
-let num1 = 4.789;
+const num1 = 4.789;
 const num2 = 12.345;
 const num3 = -8.5;
 
-let num1Rounded= Math.ceil(num1);
-Math.floor(num2);
-Math.round(num3)
-console.log(num1);
-console.log(num2);
-console.log(num3);
+console.log(Math.round(num1));
+console.log(Math.floor(num2));
+console.log(Math.ceil(num3));
 
-const nemberSet = [14, 2, -5, 101, 35, 8];
+const NumberSet = [14, 2, -5, 101, 35, 8];
