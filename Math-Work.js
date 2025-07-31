4ce93b2da0003364bc12d6d6ab0cd15ac2789692
JuniorSarh <sabelohgumede@gmail.com>
2025-07-31 14:57:42 +0200

    first commit

---
+++ Math-Work.js
@@ -0,0 +1,14 @@
+
+
+let num1 = 4.789;
+const num2 = 12.345;
+const num3 = -8.5;
+
+let num1Rounded= Math.ceil(num1);
+Math.floor(num2);
+Math.round(num3)
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+const nemberSet = [14, 2, -5, 101, 35, 8];
